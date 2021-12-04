72a7517b14edfa769b434f7ab0a69a0b2706bee9
Pratik R. Jadhav <80213864+pratikjadhav080@users.noreply.github.com>
2021-12-04 20:31:38 +0530

    unnecessary links removed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,10 +23,13 @@ export const Sidebar= ({prop,username})=>{
                     {SiderbarData.map((item, index) => {
                         return (
                             <li key={index} className={item.cName}>
-                                <Link to={item.path}>
+                                {item.path === "" ? <>
                                     <img src={item.icon} alt={item.icon}></img>
                                     <span id="spantag">{item.title}</span>
-                                </Link>
+                                </> : <Link to={item.path}>
+                                    <img src={item.icon} alt={item.icon}></img>
+                                    <span id="spantag">{item.title}</span>
+                                </Link>}
                             </li>
                         )
                     })}
